7091d9730e804bc6ef7284d45fa844d16ae92bad
billsoff <billsoff@hotmail.com>
2016-10-11 14:54:32 +0800

    Add files via upload

--- Src/TestConsole/Program.cs
+++ Src/TestConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TestConsole
@@ -11,8 +12,27 @@ namespace TestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            GetComments();
 
             Console.ReadKey();
+        }
+
+        static void GetComments()
+        {
+            Regex r = new Regex(
+@"/\*.*\*/",
+                    RegexOptions.IgnorePatternWhitespace
+                );
+            string script =
+@"/*
+function f() {
+}
+*/
+function g() {
+}";
+            Console.WriteLine(script.IndexOf(@"/*"));
+            Match m = r.Match(script);
+            Console.WriteLine(m.Value);
         }
     }
 }
