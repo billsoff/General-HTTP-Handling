d5d668c045a6dc4d2a0c45843f8445afd2de5a7a
billsoff <billsoff@hotmail.com>
2016-10-11 16:52:12 +0800

    General Service

---
+++ Src/TestConsole/Scripts/work_set/GeneralService.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="..\jquery-3.1.1.js" />
+/// <reference path="..\jquery-3.1.1.intellisense.js />
+
+function main(context) {
+    return new GeneralService(context);
+}
+
+function GeneralService(context) {
+
+}
+
+GeneralService.prototype = {
+
+};
